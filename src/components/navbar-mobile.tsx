8b798913be7fc2dd47f747fecfb6028917a9295e
Dmytro <dmitry.yurin2020@gmail.com>
2024-02-01 00:02:37 +0200

    close sheet on navigate

--- src/components/navbar-mobile.tsx
+++ src/components/navbar-mobile.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import {
   Sheet,
   SheetContent,
-  SheetDescription,
   SheetHeader,
   SheetTrigger,
 } from "@/components/ui/sheet";
@@ -13,15 +12,13 @@ import { Button } from "./ui/button";
 import Image from "next/image";
 import logo from "../../public/logo.png";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { MENU } from "@/config/menu";
 
 const NavbarMobile: React.FC = () => {
-  const pathname = usePathname();
+  const [isOpen, setIsOpen] = React.useState<boolean>(false);
 
-  React.useEffect(() => {}, [pathname]);
   return (
-    <Sheet>
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
       <SheetTrigger asChild>
         <Button variant="outline" size="icon" className="lg:hidden rounded-sm">
           <Menu className="text-slate-900" />
@@ -31,11 +28,7 @@ const NavbarMobile: React.FC = () => {
         <SheetHeader className="flex items-center">
           <div className="flex items-center gap-2">
             <Link href="/" className="flex relative h-14 w-20">
-              <Image
-                src={logo}
-                fill
-                alt="Вантажівочка логотип"
-              />
+              <Image src={logo} fill alt="Вантажівочка логотип" />
             </Link>
             <div className="flex flex-col gap-0.5 max-w-xs w-full text-start">
               <span className="font-bold uppercase text-sm">Вантажівочка</span>
@@ -51,6 +44,7 @@ const NavbarMobile: React.FC = () => {
               <li
                 key={item.id}
                 className="text-slate-900 font-bold text-lg hover:underline hover:text-muted-foreground transition-all underline-offset-8"
+                onClick={() => setIsOpen(false)}
               >
                 <a href={item.path}>{item.title}</a>
               </li>
